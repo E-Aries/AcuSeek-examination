f7e4849cbeb8cb4f0d3ed3114b2c2947d1e5e9ec
张威龙 <1768907699@qq.com>
2026-06-10 19:58:07 +0800

    chore: 清理临时脚本

--- rm_images.py
+++
@@ -1,9 +0,0 @@
-import re
-with open(r"D:\codex\examination\README.md", "r", encoding="utf-8") as f:
-    c = f.read()
-c = re.sub(r"!\[.*?\]\(screenshots/[^)]+\)\n?", "", c)
-c = re.sub(r'<img\s+[^>]*src="screenshots/[^"]+"[^>]*/>\n?', "", c)
-c = re.sub(r"\n{3,}", "\n\n", c)
-with open(r"D:\codex\examination\README.md", "w", encoding="utf-8") as f:
-    f.write(c)
-print("Done")
